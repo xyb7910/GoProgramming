1f0aa58998aca9cc879fe9e92815a383bfe8a17d
xyb7910 <1502709024@qq.com>
2024-01-11 12:32:26 +0800

    commit metadata & interpretor and create READEME.md

--- stream_grpc_test/client/main.go
+++ stream_grpc_test/client/main.go
@@ -1,34 +1,66 @@
 package main
 
 import (
-	"LearingGo/grpc_test/proto"
-	"context"
-	"fmt"
 	"google.golang.org/grpc"
+
+	"LearingGo/stream_grpc_test/proto"
+	"context"
+	_ "google.golang.org/grpc/balancer/grpclb"
+	"log"
+	"time"
+)
+
+const (
+	ADDRESS = "localhost:50052"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+	//通过grpc 库 建立一个连接
+	conn, err := grpc.Dial(ADDRESS, grpc.WithInsecure())
 	if err != nil {
-		panic(err)
+		return
 	}
 	defer conn.Close()
-
+	//通过刚刚的连接 生成一个client对象。
 	c := proto.NewGreeterClient(conn)
-	res, _ := c.GetStream(context.Background(), &proto.StreamReqData{Data: "ypc"})
+	//调用服务端推送流
+	reqstreamData := &proto.StreamReqData{Data: "aaa"}
+	res, _ := c.GetStream(context.Background(), reqstreamData)
 	for {
-		a, err := res.Recv()
+		aa, err := res.Recv()
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 			break
 		}
-		fmt.Println(a.Data)
+		log.Println(aa)
 	}
-
-	putS, _ := c.PutStream(context.Background())
-	i := 0
+	//客户端 推送 流
+	putRes, _ := c.PutStream(context.Background())
+	i := 1
 	for {
-		i ++
-		putS.Send(&)
+		i++
+		putRes.Send(&proto.StreamReqData{Data: "ss"})
+		time.Sleep(time.Second)
+		if i > 10 {
+			break
+		}
 	}
+	//服务端 客户端 双向流
+	allStr, _ := c.AllStream(context.Background())
+	go func() {
+		for {
+			data, _ := allStr.Recv()
+			log.Println(data)
+		}
+	}()
+
+	go func() {
+		for {
+			allStr.Send(&proto.StreamReqData{Data: "ssss"})
+			time.Sleep(time.Second)
+		}
+	}()
+
+	select {}
+
 }
--- stream_grpc_test/server/main.go
+++ stream_grpc_test/server/main.go
@@ -1,24 +1,27 @@
-package server
+package main
 
 import (
 	"LearingGo/stream_grpc_test/proto"
 	"fmt"
 	"google.golang.org/grpc"
+	"log"
 	"net"
+	"sync"
 	"time"
 )
 
 const PORT = ":50052"
 
-type server struct{}
+type server struct {
+}
 
+// 服务端 单向流
 func (s *server) GetStream(req *proto.StreamReqData, res proto.Greeter_GetStreamServer) error {
 	i := 0
 	for {
-		res.Send(&proto.StreamResData{
-			Data: fmt.Sprintf("%v", time.Now().Unix()),
-		})
-		time.Sleep(time.Second)
+		i++
+		res.Send(&proto.StreamResData{Data: fmt.Sprintf("%v", time.Now().Unix())})
+		time.Sleep(1 * time.Second)
 		if i > 10 {
 			break
 		}
@@ -26,30 +29,60 @@ func (s *server) GetStream(req *proto.StreamReqData, res proto.Greeter_GetStream
 	return nil
 }
 
-func (s *server) PutStream(cliStr *proto.Greeter_PutStreamClient) error {
+// 客户端 单向流
+func (s *server) PutStream(cliStr proto.Greeter_PutStreamServer) error {
+
 	for {
-		if a, err := cliStr.Recv(); err != nil {
-			fmt.Println(err)
-			break
+		if tem, err := cliStr.Recv(); err == nil {
+			log.Println(tem)
 		} else {
-			fmt.Println(a.Data)
+			log.Println("break, err :", err)
+			break
 		}
 	}
+
 	return nil
 }
-func (s *server) AllStream(allStr *proto.Greeter_AllStreamServer) error {
+
+// 客户端服务端 双向流
+func (s *server) AllStream(allStr proto.Greeter_AllStreamServer) error {
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		for {
+			data, _ := allStr.Recv()
+			log.Println(data)
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		for {
+			allStr.Send(&proto.StreamResData{Data: "ssss"})
+			time.Sleep(time.Second)
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
 	return nil
 }
 
 func main() {
+	//监听端口
 	lis, err := net.Listen("tcp", PORT)
 	if err != nil {
 		panic(err)
+		return
 	}
+	//创建一个grpc 服务器
 	s := grpc.NewServer()
+	//注册事件
 	proto.RegisterGreeterServer(s, &server{})
+	//处理链接
 	err = s.Serve(lis)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 }
