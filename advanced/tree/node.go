beb5e4315727c7190fe953a134e15597aa227e34
xyb7910 <1502709024@qq.com>
2024-01-16 22:04:37 +0800

    commit struct

--- advanced/tree/node.go
+++ advanced/tree/node.go
@@ -7,11 +7,12 @@ type Node struct {
 	Left, Right *Node
 }
 
+// (node Node)强调函数的使用者，为结构定义方法的接受者
 func (node Node) Print() {
 	fmt.Println(node.Value, " ")
 }
 
-func (node *Node) SetValue(value int) {
+func (node *Node) SetValue(value int) { //只有使用指针才可以改变结构内容
 	if node == nil {
 		fmt.Println("Setting Value to nil" + "node. Ignored.")
 		return
@@ -19,6 +20,6 @@ func (node *Node) SetValue(value int) {
 	node.Value = value
 }
 
-func CreateNode(value int) *Node {
+func CreateNode(value int) *Node { //使用工厂函数，注意返回为局部变量的地址
 	return &Node{Value: value}
 }
