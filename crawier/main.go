1313787577fb2074b372a1aff5b4a445ea289666
xyb7910 <1502709024@qq.com>
2024-01-21 23:56:34 +0800

    commit crawier

---
+++ crawier/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"LearingGo/crawier/engine"
+	"LearingGo/crawier/zhenai/parser"
+	"fmt"
+	"regexp"
+)
+
+func printCityList(contents []byte) {
+	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+	matches := re.FindAllSubmatch(contents, -1)
+
+	for _, m := range matches {
+		fmt.Printf("City: %s, URL: %s\n ", m[2], m[1])
+	}
+	fmt.Printf("Match found: %d\n",
+		len(matches))
+}
+
+func main() {
+	engine.Run(engine.Request{
+		Url:        "http://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
+}
