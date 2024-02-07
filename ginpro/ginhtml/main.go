48b0ae5833f617ffbcc4af4f9d7212dd640dd3b6
xyb7910 <1502709024@qq.com>
2024-02-07 20:41:49 +0800

    commit gorm_pro gin_html

--- ginpro/ginhtml/main.go
+++ ginpro/ginhtml/main.go
@@ -2,22 +2,43 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Article struct {
+	Title   string
+	Content string
+}
+
 func main() {
 	r := gin.Default()
 	// 加载模板
-	r.LoadHTMLGlob("templates/*")
+	r.LoadHTMLGlob("templates/**/*")
+
+	r.GET("/admin", func(c *gin.Context) {
+		c.HTML(200, "admin/index.html", gin.H{
+			"title": "admin index page",
+		})
+	})
+
+	r.GET("/admin/news", func(c *gin.Context) {
+		c.HTML(200, "admin/news.html", gin.H{
+			"title": "admin news page",
+		})
+	})
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{
-			"title": "index page",
+		c.HTML(200, "default/index.html", gin.H{
+			"title": "default index page",
 		})
 	})
 
 	r.GET("/news", func(c *gin.Context) {
-		c.HTML(200, "news.html", gin.H{
-			"title": "news page",
+		article := Article{
+			Title:   "hello",
+			Content: "hello world",
+		}
+		c.HTML(200, "default/news.html", gin.H{
+			"title": "default news page",
+			"news":  article,
 		})
 	})
-
 	r.Run()
 }
