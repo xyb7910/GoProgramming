310785b6dfe6905a6b47c401be8608b022c768d9
xyb7910 <1502709024@qq.com>
2024-02-08 19:20:06 +0800

    commit gorm_pro gin_html static loading

--- ginpro/ginhtml/main.go
+++ ginpro/ginhtml/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"time"
+)
 
 type Article struct {
 	Title   string
@@ -13,10 +18,29 @@ type ArticleList struct {
 	articles []Article
 }
 
+// UnixToTime 时间转换
+func UnixToTime(timestamp int) string {
+	fmt.Println(int64(timestamp))
+	t := time.Unix(int64(timestamp), 0)
+	return t.Format("2006-01-02 15:04:05")
+}
+
+func PrintToAdd(str1 string, str2 string) string {
+	fmt.Println(str1, str2)
+	return str1 + "---" + str2
+}
+
 func main() {
 	r := gin.Default()
+	// 自定义函数, 必须放在加载模板前
+	r.SetFuncMap(template.FuncMap{
+		"UnixToTime": UnixToTime,
+		"PrintToAdd": PrintToAdd,
+	})
 	// 加载模板
 	r.LoadHTMLGlob("templates/**/*")
+	//配置静态资源, 浏览器访问链接为 http://localhost:8080/static/css/base.css
+	r.Static("/static", "./static")
 
 	r.GET("/admin", func(c *gin.Context) {
 		c.HTML(200, "admin/index.html", gin.H{
@@ -32,7 +56,10 @@ func main() {
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(200, "default/index.html", gin.H{
-			"title": "default index page",
+			"title":     "default index page",
+			"content":   "hello world",
+			"time":      time.Now(),
+			"timestamp": 1707388414,
 		})
 	})
 
