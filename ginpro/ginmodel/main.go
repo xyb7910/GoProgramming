92382aee9d67680c075b8ee8ad868fda36a846c8
xyb7910 <1502709024@qq.com>
2024-02-09 20:48:29 +0800

    commit gorm_pro gin_model

---
+++ ginpro/ginmodel/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"LearingGo/ginpro/ginmodel/models"
+	"github.com/gin-gonic/gin"
+	"html/template"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.SetFuncMap(template.FuncMap{
+		"print": models.Print,
+	})
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.HTML(200, "ginmodel/deafult.html", gin.H{
+			"ID":      "1",
+			"message": "Hello World",
+			"func":    models.Print,
+		})
+	})
+	r.Run()
+}
---
+++ ginpro/ginmodel/models/utils.go
@@ -0,0 +1,7 @@
+package models
+
+import "fmt"
+
+func Print(str1 string, str2 string) {
+	fmt.Printf("%s\n", str1+str2)
+}
