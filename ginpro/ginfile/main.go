ed0f5772d6db78b147cb0f9f4f332ed7faec6af8
xyb7910 <1502709024@qq.com>
2024-02-10 00:02:44 +0800

    commit gorm_pro gin_upload files

--- ginpro/ginfile/main.go
+++ ginpro/ginfile/main.go
@@ -28,5 +28,30 @@ func main() {
 			"dst":      dst,
 		})
 	})
+
+	r.GET("/any", func(c *gin.Context) {
+		c.HTML(200, "templates/default.html", gin.H{})
+	})
+
+	r.POST("/uploadfiles", func(c *gin.Context) {
+		username := c.PostForm("username")
+
+		file1, err := c.FormFile("face1")
+		dst := path.Join("./static/upload", file1.Filename)
+		if err == nil {
+			c.SaveUploadedFile(file1, dst)
+		}
+		file2, err := c.FormFile("face2")
+		dst1 := path.Join("./static/upload", file2.Filename)
+		if err == nil {
+			c.SaveUploadedFile(file1, dst1)
+		}
+		c.JSON(200, gin.H{
+			"success":  true,
+			"username": username,
+			"dst":      dst,
+			"dst1":     dst1,
+		})
+	})
 	r.Run()
 }
